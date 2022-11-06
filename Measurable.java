84223b6b70e04fff89ff3b73ec5a00c46850f1c7
Istiaq Fuad <istiaqfuad123@gmail.com>
2022-11-06 20:08:03 +0600

    minor changes

--- Measurable.java
+++ Measurable.java
@@ -1,26 +1,25 @@
-public interface Measurable
-{
+public interface Measurable {
     double getMeasure();
 
     String getGradeString();
-    static double largest(Measurable[] objects){
+
+    static double largest(Measurable[] objects) {
         double max = 0;
-        for (Measurable obj : objects){
-            if (obj.getMeasure() > max){
+        for (Measurable obj : objects) {
+            if (obj.getMeasure() > max) {
                 max = obj.getMeasure();
             }
         }
         return max;
     }
 
-    static double smallest(Measurable[] objects){
+    static double smallest(Measurable[] objects) {
         double min = objects[0].getMeasure();
-        for (Measurable obj : objects){
-            if (obj.getMeasure() <= min){
+        for (Measurable obj : objects) {
+            if (obj.getMeasure() <= min) {
                 min = obj.getMeasure();
             }
         }
         return min;
     }
 }
-
